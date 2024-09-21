e000d21035e48ebe2ddcd470767d0dd25bf2a505
mcrtee <meerim.mazaripova@alatoo.edu.kg>
2024-09-22 02:00:25 +0600

    Methods for PersonalAccount Class

--- PersonalAccount.java
+++ PersonalAccount.java
@@ -11,13 +11,17 @@ public class PersonalAccount {
         this.accountNumber = accountNumber;
         this.accountHolder = accountHolder;
         this.balance = 0.0;
-
+        this.transactions = new ArrayList<>();
     }
     public void deposit(double amount) {
         if (amount > 0) {
             balance += amount;
             Amount depositTransaction = new Amount(amount, TransactionType.DEPOSIT);
             transactions.add(depositTransaction);
+            System.out.println("Deposited: $" + amount);
+        }
+        else {
+            System.out.println("Deposit amount must be positive");
         }
     }
     public void withdraw(double amount) {
@@ -25,12 +29,21 @@ public class PersonalAccount {
             balance -= amount;
             Amount withdrawalTransaction = new Amount(amount, TransactionType.WITHDRAWAL);
             transactions.add(withdrawalTransaction);
+            System.out.println("Withdraw: $" + amount);
+        }
+        else {
+            System.out.println("Invalid withdrawal amount");
         }
     }
     public void printTransactionHistory() {
         System.out.println("Transaction History for Account: " + accountNumber);
-        for (Amount transaction : transactions) {
-            System.out.println(transaction.getTransactionType() + ": $" + transaction.getAmount());
+        if (transactions.isEmpty()) {
+            System.out.println("No transactions availale");
+        }
+        else {
+            for (Amount transaction : transactions) {
+                System.out.println(transaction.getTransactionType() + ": $" + transaction.getAmount());
+            }
         }
     }
     public double getBalance() {
